aa2445b78d6d7a34042761c0750e3cae3b4bbd94
yann_dm <yann.montmarin@gmail.com>
2020-10-26 13:52:32 +0100

    Discard unusefull option gnu++11 (urdfdom does not nedd it anymore)

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,8 +23,7 @@ cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_CXX_FLAGS=-std=gnu++11
+      -DCMAKE_INSTALL_LIBDIR=lib
 make -j${CPU_COUNT} 
 make install
 
